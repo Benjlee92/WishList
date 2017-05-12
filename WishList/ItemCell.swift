2aa6e1b10766a5c1b0c86c4fb4d56b14fe6a2416
Benjamin Lee <benjlee92@gmail.com>
2017-05-11 21:58:43 -0700

    Added FRC / boilerplate

---
+++ WishList/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  WishList
+//
+//  Created by Ben on 5/11/17.
+//  Copyright © 2017 Ben. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+}
